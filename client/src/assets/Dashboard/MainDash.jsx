63ee82bd83ef34cd51b472040d7035f60aec7eae
dano-chiteia02 <157301664+dano-chiteia02@users.noreply.github.com>
2025-03-30 20:28:43 +0100

    Dash layout

--- client/src/assets/Dashboard/MainDash.jsx
+++ client/src/assets/Dashboard/MainDash.jsx
@@ -7,15 +7,41 @@ const MainDash = () => {
     <>
         <div className='Box-main border'>
             <DashHeader/>
-           <div className='flex-space-between'>
-            <h1 className='ml-3'>
-                VISÃO <br/> GERAL
-            </h1>
-                <div className='elements flex-center'>
-                    <div className='elemensts_value'>
-                        <img className='' src={empresa_bg_br} alt="" />
-                        <p>7 empresas associados</p>
-                    </div>
+           <div className='flex'>
+                <h1 className='ml-3'>
+                    VISÃO <br/> GERAL
+                </h1>
+                <div className=' broke-line'>
+                        <div className='elements flex-center mx-1 mb-2'>
+                            <div className='elemensts_value'>
+                                <img className='' src={empresa_bg_br} alt="" />
+                                <p>7 empresas associados</p>
+                            </div>
+                        </div>
+                        <div className='elements flex-center mx-1 mb-2'>
+                            <div className='elemensts_value'>
+                                <img className='' src={empresa_bg_br} alt="" />
+                                <p>7 empresas associados</p>
+                            </div>
+                        </div>
+                        <div className='elements flex-center mx-1 mb-2'>
+                            <div className='elemensts_value'>
+                                <img className='' src={empresa_bg_br} alt="" />
+                                <p>7 empresas associados</p>
+                            </div>
+                        </div>
+                        <div className='elements flex-center mx-1 mb-2'>
+                            <div className='elemensts_value'>
+                                <img className='' src={empresa_bg_br} alt="" />
+                                <p>7 empresas associados</p>
+                            </div>
+                        </div>
+                        <div className='elements flex-center mx-1 mb-2'>
+                            <div className='elemensts_value'>
+                                <img className='' src={empresa_bg_br} alt="" />
+                                <p>7 empresas associados</p>
+                            </div>
+                        </div>
                 </div>
             </div>
             
